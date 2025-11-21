20577ae679728c681e32f4b5350e6a49113c74eb
David Dong <david041123@icloud.com>
2025-11-20 22:01:54 -0500

    populate playlist song

---
+++ server/pop_playlist_songs.sql
@@ -0,0 +1,20 @@
+INSERT INTO playlist_song (plstid, sid, position, added_at) VALUES
+    (2001, '0006YMdmPphDXTlUBHGr6Q', 1, '2025-02-10 08:00:00'),
+    (2001, '000cYQJyEbqfUm6Ls0Xs4s', 2, '2025-02-10 08:05:00'),
+    (2001, '000tOaXqLPh2Vce0YOpcui', 3, '2025-02-10 08:10:00'),
+
+    (2005, '000xQL6tZNLJzIrtIgxqSl', 1, '2025-02-10 09:00:00'),
+    (2005, '001UkMQHw4zXfFNdKpwXAF', 2, '2025-02-10 09:05:00'),
+    (2005, '002xuBMSqq1a8gGk2upLXz', 3, '2025-02-10 09:10:00'),
+
+    (2010, '003byyqUUr7sJdwSbJOE1Y', 1, '2025-02-10 10:00:00'),
+    (2010, '003fztMsGNg91fl99yrDvt', 2, '2025-02-10 10:05:00'),
+    (2010, '003iCn06wxW5qYmHiP4jT0', 3, '2025-02-10 10:10:00'),
+
+    (2018, '004Ja2CXmd6PwpyKYR8z9Z', 1, '2025-02-10 11:00:00'),
+    (2018, '004MGWWZ18SSmcryRCDosb', 2, '2025-02-10 11:05:00'),
+    (2018, '004MiktqrlEPng9SDw4N9d', 3, '2025-02-10 11:10:00'),
+
+    (2023, '004VvDQRUC6Pj0Ol0zAT4D', 1, '2025-02-10 12:00:00'),
+    (2023, '0053RngWZ29LSOeHfHKb25', 2, '2025-02-10 12:05:00'),
+    (2023, '005FsER7NgcTZ3uyi9tviH', 3, '2025-02-10 12:10:00');
