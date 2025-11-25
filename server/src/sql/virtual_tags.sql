5b6e0d9cf41c8b8362fc48e296c2d0248b2f7d97
David Dong <david041123@icloud.com>
2025-11-25 17:10:19 -0500

    edited viral_tag.sql

--- server/src/sql/virtual_tags.sql
+++ server/src/sql/virtual_tags.sql
@@ -1,9 +1,8 @@
 CREATE OR REPLACE VIEW virt_song_tag AS
     SELECT sid, name AS song_name, 1 AS tag
     FROM songs
-    WHERE danceability >= 0.60
+    WHERE danceability >= 0.50
       AND energy >= 0.70
-      AND valence >= 0.50
       AND tempo BETWEEN 110 AND 150
       AND loudness > -7
       AND `mode` = 1
@@ -11,7 +10,7 @@ CREATE OR REPLACE VIEW virt_song_tag AS
 UNION ALL
     SELECT sid, name, 2
     FROM songs
-    WHERE energy < 0.50
+    WHERE energy < 0.60
       AND acousticness > 0.40
       AND tempo BETWEEN 60 AND 110
       AND loudness < -8
@@ -21,7 +20,6 @@ UNION ALL
     FROM songs
     WHERE valence < 0.35
       AND energy < 0.50
-      AND tempo < 110
       AND loudness < -8
       AND `mode` = 0
 
@@ -30,7 +28,6 @@ UNION ALL
     FROM songs
     WHERE energy >= 0.75
       AND danceability >= 0.55
-      AND tempo BETWEEN 120 AND 180
       AND loudness > -6
 
 UNION ALL
@@ -38,7 +35,6 @@ UNION ALL
     FROM songs
     WHERE energy < 0.30
       AND speechiness < 0.20
-      AND acousticness > 0.60
       AND loudness < -12
       AND tempo BETWEEN 40 AND 90
 
@@ -48,7 +44,6 @@ UNION ALL
     WHERE valence BETWEEN 0.40 AND 0.70
       AND energy BETWEEN 0.30 AND 0.60
       AND acousticness > 0.25
-      AND tempo BETWEEN 60 AND 100
       AND loudness < -6;
 
 
